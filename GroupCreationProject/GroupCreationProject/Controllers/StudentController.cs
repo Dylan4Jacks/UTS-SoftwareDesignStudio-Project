2efcccc32eaa45d039e53be35c1daf99a20dc0e0
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2022-09-09 14:44:58 +1000

    View Update

---
+++ GroupCreationProject/GroupCreationProject/Controllers/StudentController.cs
@@ -0,0 +1,32 @@
+﻿using GroupCreationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GroupCreationProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        private readonly ILogger<StudentController> _logger;
+
+        public StudentController(ILogger<StudentController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}

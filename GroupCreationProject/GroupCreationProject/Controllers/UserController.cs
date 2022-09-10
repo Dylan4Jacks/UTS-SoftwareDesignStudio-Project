fab1f8342c58a289de93f007e3cb428c620a02f6
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2022-09-10 17:59:51 +1000

    Testing Frontend Controller and View

---
+++ GroupCreationProject/GroupCreationProject/Controllers/UserController.cs
@@ -0,0 +1,60 @@
+﻿using GroupCreationProject.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GroupCreationProject.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<UserController> _logger;
+
+        private static List<StudentModelTemp> students = new List<StudentModelTemp>() {
+            new StudentModelTemp() {
+                Id=1,
+                Name="Barbra-Ann",
+                GroupId=4,
+                Preferences="A group with few students",
+                Background=StudentModelTemp.Backgrounds.Design,
+                Interest=StudentModelTemp.Interests.Research,
+                capability=StudentModelTemp.Capabilities.Frontend
+
+            },
+            new StudentModelTemp() {
+                Id=2,
+                Name="Rachel",
+                GroupId=4,
+                Preferences="I want a HD(85%)",
+                Background=StudentModelTemp.Backgrounds.Design,
+                Interest=StudentModelTemp.Interests.Management,
+                capability=StudentModelTemp.Capabilities.Servers
+            },
+            new StudentModelTemp() {
+                Id=3,
+                Name="Angela",
+                GroupId=3,
+                Background=StudentModelTemp.Backgrounds.Programming,
+                Interest=StudentModelTemp.Interests.UserExperience,
+                capability=StudentModelTemp.Capabilities.Routing
+            },
+        };
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult Teacher()
+        {
+            return View();
+        }
+        public IActionResult Student()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ GroupCreationProject/GroupCreationProject/Models/StudentModelTemp.cs
@@ -0,0 +1,33 @@
+﻿namespace GroupCreationProject.Models
+{
+    public class StudentModelTemp
+    {
+        public enum Backgrounds
+        {
+            Programming,
+            Design
+            
+        }
+        public enum Interests
+        {
+            Management,
+            Technical,
+            Research,
+            UserExperience
+        }
+        public enum Capabilities
+        {
+            Servers,
+            Routing,
+            Frontend,
+        }
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int? GroupId { get; set; }
+        public String? Preferences { get; set; }
+        public Backgrounds? Background { get; set; }
+        public Interests? Interest { get; set; }
+        public Capabilities? capability { get; set; }
+    }
+}

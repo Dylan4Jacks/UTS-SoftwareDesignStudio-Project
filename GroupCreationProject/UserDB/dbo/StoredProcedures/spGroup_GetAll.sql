d85277ad8a3ce336cf8d4880a75f50cabc9d394f
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-09-24 15:09:07 +1000

    Group Table and Functions Added

---
+++ GroupCreationProject/UserDB/dbo/StoredProcedures/spGroup_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spGroup_GetAll]
+AS
+BEGIN
+	SELECT GroupId, GroupName
+	FROM dbo.[Group];
+END
--- GroupCreationProject/UserDB/dbo/Tables/Student.sql
+++ GroupCreationProject/UserDB/dbo/Tables/Student.sql
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream:GroupCreationProject/UserDB/dbo/Tables/Student.sql
 ﻿CREATE TABLE [dbo].[User]
 (
 	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
@@ -7,3 +8,15 @@
     [Preferences] NVARCHAR(100) NOT NULL,
     [Skills] NVARCHAR(100) NOT NULL
 )
+=======
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [FirstName] NVARCHAR(50) NOT NULL, 
+    [LastName] NVARCHAR(50) NOT NULL,
+    [Email] NVARCHAR(100) NOT NULL,
+    [Preferences] NVARCHAR(100) NOT NULL,
+    [Skills] NVARCHAR(100) NOT NULL,
+    CONSTRAINT [GroupId] FOREIGN KEY (GroupId) REFERENCES [Group] (GroupId)
+)
+>>>>>>> Stashed changes:GroupCreationProject/UserDB/dbo/Tables/User.sql

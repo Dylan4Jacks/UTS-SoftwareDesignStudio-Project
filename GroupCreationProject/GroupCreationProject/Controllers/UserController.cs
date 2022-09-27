99d7255a0e4c86d4822f0085116caafb6989544f
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2022-09-27 22:46:27 +1000

    removed demo data

--- GroupCreationProject/GroupCreationProject/Controllers/UserController.cs
+++ GroupCreationProject/GroupCreationProject/Controllers/UserController.cs
@@ -8,36 +8,6 @@ namespace GroupCreationProject.Controllers
     {
         private readonly ILogger<UserController> _logger;
 
-        private static List<StudentModelTemp> students = new List<StudentModelTemp>() {
-            new StudentModelTemp() {
-                Id=1,
-                Name="Barbra-Ann",
-                GroupId=4,
-                Preferences="A group with few students",
-                Background=StudentModelTemp.Backgrounds.Design,
-                Interest=StudentModelTemp.Interests.Research,
-                capability=StudentModelTemp.Capabilities.Frontend
-
-            },
-            new StudentModelTemp() {
-                Id=2,
-                Name="Rachel",
-                GroupId=4,
-                Preferences="I want a HD(85%)",
-                Background=StudentModelTemp.Backgrounds.Design,
-                Interest=StudentModelTemp.Interests.Management,
-                capability=StudentModelTemp.Capabilities.Servers
-            },
-            new StudentModelTemp() {
-                Id=3,
-                Name="Angela",
-                GroupId=3,
-                Background=StudentModelTemp.Backgrounds.Programming,
-                Interest=StudentModelTemp.Interests.UserExperience,
-                capability=StudentModelTemp.Capabilities.Routing
-            },
-        };
-
         public UserController(ILogger<UserController> logger)
         {
             _logger = logger;

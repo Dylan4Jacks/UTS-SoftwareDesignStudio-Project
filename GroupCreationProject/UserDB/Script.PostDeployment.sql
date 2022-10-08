f1304f12da735f23888d8ebb71df451b7ced41ed
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2022-10-08 17:58:32 +1100

    Testing Different PostDeployment Content

--- GroupCreationProject/UserDB/Script.PostDeployment.sql
+++ GroupCreationProject/UserDB/Script.PostDeployment.sql
@@ -1,18 +1,29 @@
 ﻿if not exists (select 1 from dbo.[Student])
 begin
+	insert into dbo.[Group] (GroupName, Details)
+	values ('a', 'Apple'), ('b', 'Apple'), ('c', 'Apple'), ('d', 'Apple'), ('e', 'Apple');
+
 	insert into dbo.[Student] (FirstName, LastName, Email, Password, GroupId)
 	values ('John', 'Smith', 'John.Smith@gmail.com', 'a', null),
 	('Mary', 'Jane', 'Mary.Jane@gmail.com', 'b', 1),
 	('Sue', 'Solomon', 'Sue.Solomon@gmail.com', 'c', null),
 	('Kate', 'Allen', 'Kate.Allen@gmail.com', 'd', 1);
 
-	insert into dbo.[CategoryList] (Name, Details)
-	values ('A', null);
+	--insert into dbo.[CategoryList] (Name, Details)
+	--values ('A', null);
 
-	insert into dbo.[CategoryItem] (Name, Details, CategoryListId)
-	values ('Apple', null, 1);
+	--insert into dbo.[CategoryItem] (Name, Details, CategoryListId)
+	--Select 'Apple', null, catList.CategoryListId
+	--FROM dbo.[CategoryList] catList 
+	--where catList.Name = 'A';
 
-	insert into dbo.[CategorySelection] (StudentId, CategoryItemId, Content)
-	values (1, 1, null);
+	--INSERT INTO dbo.[CategorySelection] (StudentId, CategoryItemId, Content)
+	--SELECT name, stu.StudentId, cat.CategoryItemId, null
+	--FROM dbo.[Student] stu, dbo.[CategoryItem] cat
+	--where stu.Email = 'John.Smith@gmail.com' AND stu.Password = 'a'
+	--AND cat.Name= 'Apple';
 
+	--DELETE
+	--FROM dbo.[Student]
+	--WHERE Email = 'John.Smith@gmail.com' AND Password = 'a';
 end

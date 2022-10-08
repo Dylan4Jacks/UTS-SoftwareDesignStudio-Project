d4201a7aae00616adab7a7187c8f6c4bbdeee03a
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-10-08 16:16:09 +1100

    Student Procedures Added

---
+++ GroupCreationProject/UserDB/dbo/StoredProcedures/Student/spStudent_Get.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spStudent_Get]
+	@StudentId int,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@Email nvarchar(100),
+	@Password nvarchar(50),
+	@GroupId int
+AS
+BEGIN
+	SELECT StudentId, FirstName, LastName, Email, Password, GroupId
+	FROM dbo.[Student]
+	WHERE StudentId = @StudentId;
+END
---
+++ GroupCreationProject/UserDB/dbo/StoredProcedures/Student/spStudent_Update.sql
@@ -0,0 +1,13 @@
+﻿CREATE PROCEDURE [dbo].[spStudent_Update]
+	@StudentId int,
+	@FirstName nvarchar(50),
+	@LastName nvarchar(50),
+	@Email nvarchar(100),
+	@Password nvarchar(50),
+	@GroupId int
+AS
+BEGIN
+	UPDATE dbo.[Student]
+	SET FirstName = @FirstName, LastName = @LastName, Email = @Email, @Password = Password, @GroupId = GroupId
+	WHERE StudentId = @StudentId
+END

8b022f279183b39225a4249978f11e3f89bb6f33
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-10-08 16:39:57 +1100

    Teacher Procedures Added

---
+++ GroupCreationProject/UserDB/dbo/StoredProcedures/Teacher/spTeacher_Delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spTeacher_Delete]
+	@TeacherId int
+AS
+BEGIN
+	DELETE
+	FROM dbo.[Teacher]
+	WHERE TeacherId = @TeacherId;
+END
---
+++ GroupCreationProject/UserDB/dbo/StoredProcedures/Teacher/spTeacher_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spTeacher_GetAll]
+AS
+BEGIN
+	SELECT TeacherId, FirstName, LastName, Email, Password
+	FROM dbo.[Teacher];
+END

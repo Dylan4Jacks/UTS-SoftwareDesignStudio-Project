4bdd32ea466138ac335bcd42690a12089fb148cc
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2022-10-08 23:36:07 +1100

    Removed GroupCreationAPI2
    If this API2 is required we will revert the changes made

--- GroupCreationProject/DataAccess/Data/IteacherData.cs
+++ GroupCreationProject/DataAccess/Data/IteacherData.cs
@@ -8,5 +8,5 @@ public interface ITeacherData
     Task<IEnumerable<TeacherModel>> GetTeachers();
     Task InsertTeacher(TeacherModel teacher);
     Task UpdateTeacher(TeacherModel teacher);
-    Task<teacherModel?> loginTeacher(string email, string password);
+    Task<TeacherModel?> loginTeacher(string email, string password);
 }

5dbe5053392bb45ce79337879961251a587bf21c
Perry <79495144+Perry-byte@users.noreply.github.com>
2022-10-08 15:50:36 +1100

    Category Item Procedures Added

---
+++ GroupCreationProject/UserDB/dbo/StoredProcedures/Category Item/spCategoryItem_Delete.sql
@@ -0,0 +1,8 @@
+﻿CREATE PROCEDURE [dbo].[spCategoryItem_Delete]
+	@CategoryItemId int
+AS
+BEGIN
+	DELETE
+	FROM dbo.[CategoryItem]
+	WHERE CategoryItemId = @CategoryItemId;
+END
---
+++ GroupCreationProject/UserDB/dbo/StoredProcedures/Category Item/spCategoryItem_GetAll.sql
@@ -0,0 +1,6 @@
+﻿CREATE PROCEDURE [dbo].[spCategoryItem_GetAll]
+AS
+BEGIN
+	SELECT CategoryItemId, Name, Details, CategoryListId
+	FROM dbo.[CategoryItem];
+END

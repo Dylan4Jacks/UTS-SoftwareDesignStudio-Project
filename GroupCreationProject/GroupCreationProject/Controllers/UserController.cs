23a258bedd90904de049163dcd15589d85c8fe34
Dylan Jack <48197729+Dylan4Jacks@users.noreply.github.com>
2022-10-23 16:46:58 +1100

    changes

--- GroupCreationProject/GroupCreationProject/Controllers/UserController.cs
+++ GroupCreationProject/GroupCreationProject/Controllers/UserController.cs
@@ -16,29 +16,14 @@ namespace GroupCreationProject.Controllers
         }
         public IActionResult Teacher()
         {
-            return View();
+            List<StudentModel> students = new List<StudentModel>() { new StudentModel() { StudentId = 1, FirstName = "Ann", LastName = "Mo", Email = "Ann@Gmail.com", Password = "12345" } };
+            return View(students);
         }
         public IActionResult Student()
         {
             return View();
         }
 
-        public IActionResult StudentsPartial()
-        {
-            List<StudentModel> students = new List<StudentModel>() { new StudentModel(){ StudentId = 1, FirstName = "Ann", LastName = "Mo", Email = "Ann@Gmail.com", Password = "12345" } };
-            return PartialView("_StudentsPartial", students);
-        }
-
-        public IActionResult GroupsPartial()
-        {
-            List<GroupModel> groups = new List<GroupModel>() { new GroupModel() { GroupId = 1, GroupName = "A" } };
-            return PartialView("_GroupsPartial", groups);
-        }
-
-        public IActionResult CategoriesPartial()
-        {
-            return PartialView("_CategoriesPartial");
-        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
